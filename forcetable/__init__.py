49551588287d1a222b69e11366fb58278b795702
Kevin Sekgobela <kevinnoko23@gmail.com>
2022-10-23 08:18:30 +0200

    Added package __all__(#4)

--- forcetable/__init__.py
+++ forcetable/__init__.py
@@ -18,5 +18,37 @@ from forcetable._table import csv_to_table_fp
 from forcetable._table import csv_to_table
 
 
+__all__ = [
+    'record', 
+    'field', 
+    'file_field', 
+
+    'table', 
+    'records_table', 
+    'primary_table', 
+
+    'records_to_dicts', 
+    'records_to_fields', 
+    'records_to_table',
+
+    'dicts_to_records', 
+    'extract_record_primary_item', 
+
+    'json_to_table', 
+    'csv_to_table_fp', 
+    'csv_to_table'
+]
+
+# # Setups __all__ containing public symbols and non modules symbols.
+# # This excludes modules from being imported when importing with 'import *'.
+# import types
+# __all__ = [
+#     name for name, object_ in globals().items()
+#     if not (name.startswith("_") or isinstance(object_, types.ModuleType))
+# ]
+# # 'types' module is no longer needed.
+# del types
+
+
 __name__ = "forcetable"
-___version__ = "0.0.2"
+__version__ = "0.0.5"
